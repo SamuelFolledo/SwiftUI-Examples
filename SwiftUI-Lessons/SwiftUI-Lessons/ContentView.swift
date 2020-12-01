b43bd1e1d5fa9400467c4cf6f09b668323b26a7f
Samuel Folledo <samuelfolledo@gmail.com>
2020-11-30 23:22:49 -0500

    [add] a simple tableview

--- SwiftUI-Lessons/SwiftUI-Lessons/ContentView.swift
+++ SwiftUI-Lessons/SwiftUI-Lessons/ContentView.swift
@@ -8,14 +8,23 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let pets = ["Doggie", "Cathy", "Tweetie", "Bobina", "Feeshy"]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        List {
+            ForEach(pets, id: \.self) {
+                Text($0)
+            }
+            Text("Hello, world!")
+                .padding()
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .previewDevice("iPhone 8")
     }
 }
