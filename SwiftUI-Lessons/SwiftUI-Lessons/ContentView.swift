5375a913ea012820e40bae336fd692a567c84a94
Samuel Folledo <samuelfolledo@gmail.com>
2020-11-30 23:39:42 -0500

    [add] labels

--- SwiftUI-Lessons/SwiftUI-Lessons/ContentView.swift
+++ SwiftUI-Lessons/SwiftUI-Lessons/ContentView.swift
@@ -27,12 +27,30 @@ struct ContentView: View {
                 }
             }
         }
+        
+        VStack {
+            Text("Love Yourz")
+                .bold()
+            Text("It's beauty in the struggle, ugliness in the success\nHear my words or listen to my signal of distressI grew up in the city and though some times we had less\nCompared to some of my n---as down the block, man, we were blessed.")
+//                .lineLimit(2)
+                .minimumScaleFactor(8)
+                .multilineTextAlignment(.center)
+                .truncationMode(.middle)
+                .foregroundColor(.red)
+                .padding()
+                .background(Color.gray)
+                .border(Color.black, width: 3)
+        }
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
-            .previewDevice("iPhone 8")
+        Group {
+            ContentView()
+                .previewDevice("iPhone 8")
+            ContentView()
+                .previewDevice("iPhone 8")
+        }
     }
 }
