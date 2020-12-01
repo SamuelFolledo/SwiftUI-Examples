e614252f997fcb6bba1500e42b853b5ee422a9c2
Samuel Folledo <samuelfolledo@gmail.com>
2020-11-30 23:22:30 -0500

    [add] started a SwiftUI app

---
+++ SwiftUI-Lessons/SwiftUI-Lessons/SwiftUI_LessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_LessonsApp.swift
+//  SwiftUI-Lessons
+//
+//  Created by Samuel Folledo on 11/30/20.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_LessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
