c91233bab44c370a46f399058733eceb9a4ff879
Samuel Folledo <samuelfolledo@gmail.com>
2020-12-15 23:22:53 -0500

    [add] new view file for stacsk

---
+++ SwiftUI-Lessons/SwiftUI-Lessons/StackViews.swift
@@ -0,0 +1,20 @@
+//
+//  Stacks.swift
+//  SwiftUI-Lessons
+//
+//  Created by Samuel Folledo on 12/15/20.
+//
+
+import SwiftUI
+
+struct StackViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StackViews_Previews: PreviewProvider {
+    static var previews: some View {
+        StackViews()
+    }
+}
