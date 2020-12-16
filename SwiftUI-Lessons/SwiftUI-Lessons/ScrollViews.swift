7ff37a57b2d03699ffe2ddfd038e19db642d8e45
Samuel Folledo <samuelfolledo@gmail.com>
2020-12-16 08:06:35 -0500

    [add] scrollview

---
+++ SwiftUI-Lessons/SwiftUI-Lessons/ScrollViews.swift
@@ -0,0 +1,20 @@
+//
+//  ScrollViews.swift
+//  SwiftUI-Lessons
+//
+//  Created by Samuel Folledo on 12/16/20.
+//
+
+import SwiftUI
+
+struct ScrollViews: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ScrollViews_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollViews()
+    }
+}
